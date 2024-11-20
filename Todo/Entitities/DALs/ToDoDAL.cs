502c719c32e8cc60b3fac4bf08a7cf540b0d03d8
Robert Garner <rgarner011235@gmail.com>
2024-11-19 18:59:52 -0700

    fixed sql injection

--- Todo/Entitities/DALs/ToDoDAL.cs
+++ Todo/Entitities/DALs/ToDoDAL.cs
@@ -47,8 +47,14 @@ namespace Todo.Entitities.DALs
             string connStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Rob011235\\source\\repos\\Tutorials\\Todo\\Todo\\ToDoDB.mdf;Integrated Security=True";
             using (SqlConnection conn = new SqlConnection(connStr))
             {
-                string commStr = $"INSERT INTO ToDoItem VALUES ('{item.Id}', '{item.Title}', '{item.Description}', {(item.IsDone?1:0)}, '{item.CompletionDate}');";
+                //string commStr = $"INSERT INTO ToDoItem VALUES ('{item.Id}', '{item.Title}', '{item.Description}', {(item.IsDone?1:0)}, '{item.CompletionDate}');";
+                string commStr = $"INSERT INTO ToDoItem VALUES (@Id, @Title, @Description, @IsDone, @CompletionDate);";
                 SqlCommand cmd = new SqlCommand(commStr, conn);
+                cmd.Parameters.AddWithValue("Id", item.Id);
+                cmd.Parameters.AddWithValue ("Title", item.Title);
+                cmd.Parameters.AddWithValue ("Description", item.Description);
+                cmd.Parameters.AddWithValue("IsDone",item.IsDone);
+                cmd.Parameters.AddWithValue("CompletionDate", item.CompletionDate);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
